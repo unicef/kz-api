8a0b01e995529ec913ac40f702eb5cde9663e672
Alexandr Sadirin <alexander.sadirin@iskyweb.com>
2019-06-07 10:22:01 +0300

    Update 1559818243015_test-migration-deployment.js

--- migrations/1559818243015_test-migration-deployment.js
+++ migrations/1559818243015_test-migration-deployment.js
@@ -1,7 +1,7 @@
 exports.shorthands = undefined;
 
 exports.up = (pgm) => {
-    pgm.sql('INSERT INTO public.partner_types (title) VALUES("test_deploy_migration")');
+    pgm.sql('INSERT INTO public.partner_types (title) VALUES ("test_deploy_migration")');
 };
 
 exports.down = (pgm) => {
