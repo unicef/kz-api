bb086fa073a1b29a9307c8b0e36b8b6712713a5f
Andrii Tokariev <andrii.tokariev@iskyweb.com>
2019-06-12 16:34:06 +0300

    fix(migrations): fix translation migration

--- migrations/1560228130535_new-translations-seed.js
+++ migrations/1560228130535_new-translations-seed.js
@@ -6,7 +6,7 @@ exports.up = (pgm) => {
     pgm.sql("INSERT INTO public.translations (\"key\", ru, en) VALUES('userDetails.title', 'Детали о пользователе', 'User Details')");
     pgm.sql("INSERT INTO public.translations (\"key\", ru, en) VALUES('common.fields.email', 'Email', 'Email')");
     pgm.sql("INSERT INTO public.translations (\"key\", ru, en) VALUES('common.btns.save', 'Сохранить', 'Save')");
-    pgm.sql(pgm.sql("INSERT INTO public.translations (\"key\", ru, en) VALUES('common.btns.cancel', 'Отмена', 'Cancel')");
+    pgm.sql("INSERT INTO public.translations (\"key\", ru, en) VALUES('common.btns.cancel', 'Отмена', 'Cancel')");
     pgm.sql("INSERT INTO public.translations (\"key\", ru, en) VALUES('common.btns.next', 'Далее', 'Next')");
     pgm.sql("INSERT INTO public.translations (\"key\", ru, en) VALUES('common.btns.back', 'Назад', 'Back')");
     pgm.sql("INSERT INTO public.translations (\"key\", ru, en) VALUES('common.fields.validation.field.required', 'Поле является обязательным', 'Field us required')");
