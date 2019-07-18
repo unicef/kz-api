05fba917709a183480b717c4b62c96eaaa891b0a
Andrii Tokariev <andrii.tokariev@iskyweb.com>
2019-07-18 16:24:24 +0300

    deploy edit

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,7 +6,7 @@ if [ "$NODE_ENV" == "production" ] ; then
    npm run start
 else
   if [ "$NODE_ENV" == "test" ] ; then
-     npm run start
+     npm run start && docsify serve docs -p 3030
   else
       npm run dev
   fi
