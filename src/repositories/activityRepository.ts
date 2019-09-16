2fe3d25241aa37e8798bc7ebc8da626d86ae678a
Andrii Tokariev <andrii.tokariev@iskyweb.com>
2019-09-16 11:55:17 +0300

    fix(get activities) : fix errors with amounts and total amounts param

--- src/repositories/activityRepository.ts
+++ src/repositories/activityRepository.ts
@@ -6,7 +6,16 @@ import Role from "../models/role";
 
 class ActivityRepository {
     static getByProjectId = async (projectId: number) => {
-        const query = `SELECT pa."id" as "id", pa."title" as "title" FROM project_activities "pa" WHERE pa."projectId" = ${projectId}`;
+        const query = `SELECT 
+            pa."id" as "id",
+            pa."title" as "title", 
+            0 as "amountE", 
+            0 AS "amountF", 
+            0 AS "amountG",
+            false as "isRejected",
+            '' AS "rejectReason"
+            FROM project_activities "pa" 
+            WHERE pa."projectId" = ${projectId}`;
 
         const activities = await sequelize.query(query,{type: QueryTypes.SELECT});
 
@@ -32,7 +41,12 @@ class ActivityRepository {
     }
 
     static getTotalRequestAmounts = async (requestId: number) => {
-        const query = `SELECT SUM(ra."amountE") as "totalE", SUM(ra."amountF") as "totalF", SUM(ra."amountG") as "totalG" FROM request_activities "ra" WHERE ra."requestId" = ${requestId}`;
+        const query = `SELECT 
+            CASE WHEN SUM(ra."amountE") IS NULL THEN 0 ELSE SUM(ra."amountE") END AS "totalE",
+            CASE WHEN SUM(ra."amountF") IS NULL THEN 0 ELSE SUM(ra."amountF") END AS "totalF",
+            CASE WHEN SUM(ra."amountG") IS NULL THEN 0 ELSE SUM(ra."amountG") END AS "totalG"
+            FROM request_activities "ra" 
+            WHERE ra."requestId" = ${requestId}`;
 
         const total = await sequelize.query(query,{
             type: QueryTypes.SELECT,
